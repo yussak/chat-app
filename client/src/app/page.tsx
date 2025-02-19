1bab434d64e886e333e71a8d674edac40b118cd2
yussak <yusuke.sakuraba0213@gmail.com>
2025-02-19 22:26:12 +0900

    create, index, deleteできるようにした

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,29 @@
 "use client";
 
+import axios from "axios";
 import { signIn, signOut, useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const { data: session } = useSession();
+
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState("");
+
+  const fetchtodos = () => axios.get("http://localhost:8080/");
+  const postMessage = (name: string) =>
+    axios.post("http://localhost:8080/add", { name });
+
+  useEffect(() => {
+    fetchtodos().then((res) => setTodos(res.data));
+  }, []);
+
+  const handleSend = async () => {
+    if (!input.trim()) return;
+    const res = await postMessage(input);
+    setTodos([...todos, res.data]);
+    setInput("");
+  };
 
   return (
     <div>
@@ -11,6 +31,36 @@ export default function Home() {
         <>
           <h1>Welcome, {session.user?.name}</h1>
           <button onClick={() => signOut()}>Sign out</button>
+
+          <ul>
+            {todos.map((todo) => (
+              <li key={todo.ID}>
+                {todo.ID}
+                {todo.Name}
+                {/* 削除ボタン */}
+                <button
+                  onClick={async () => {
+                    await axios.delete(`http://localhost:8080/delete`, {
+                      params: { id: todo.ID },
+                    });
+                    setTodos(todos.filter((t) => t.ID !== todo.ID));
+                  }}
+                >
+                  del
+                </button>
+              </li>
+            ))}
+          </ul>
+
+          <div>
+            <input
+              type="text"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              placeholder="メッセージを入力..."
+            />
+            <button onClick={handleSend}>送信</button>
+          </div>
         </>
       ) : (
         <>
