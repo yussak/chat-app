da8c3a499f092f92ad9d2d3322fa22cd7584f5b0
yussak <yusuke.sakuraba0213@gmail.com>
2025-04-05 11:41:46 +0900

    fix error

--- client/src/app/workspaces/[workspaceId]/layout.tsx
+++ client/src/app/workspaces/[workspaceId]/layout.tsx
@@ -2,16 +2,18 @@
 import Sidebar from "@/app/components/Sidebar";
 import React from "react";
 
-export default function WorkspaceLayout({
+export default async function WorkspaceLayout({
   children,
   params,
 }: {
   children: React.ReactNode;
   params: { workspaceId: string };
 }) {
+  // paramsが用意されるのを待つ
+  const { workspaceId } = await params;
   return (
     <div style={{ display: "flex", height: "100vh" }}>
-      <Sidebar workspaceId={params.workspaceId} />
+      <Sidebar workspaceId={workspaceId} />
       <main style={{ flex: 1 }}>{children}</main>
     </div>
   );
