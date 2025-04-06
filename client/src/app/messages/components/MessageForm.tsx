699dd42c87bd5557b279d703650a993388096cf4
yussak <yusuke.sakuraba0213@gmail.com>
2025-04-06 11:34:31 +0900

    モックのボタン追加

--- client/src/app/messages/components/MessageForm.tsx
+++ client/src/app/messages/components/MessageForm.tsx
@@ -25,16 +25,26 @@ export const MessageForm = ({
   >
     {/* 上段ボタン */}
     <div className="flex gap-2 mb-2">
+      {/* todo:太字実装 */}
+      <button type="button" className="hover:bg-gray-200 font-bold">
+        B
+      </button>
+      {/* todo:実装 */}
+      <button type="button" className="px-2 hover:bg-gray-200 italic">
+        I
+      </button>
       <button
         type="button"
         onClick={handleStrikethrough}
-        className="px-4 py-2 bg-gray-100 rounded hover:bg-gray-200"
+        className="px-2 rounded hover:bg-gray-200"
       >
         {/* 打ち消し線 */}
         <span className="relative font-bold text-sm">
           S
           <span className="absolute left-0 right-0 top-1/2 border-t border-black rotate-[15deg]"></span>
         </span>
+
+        <span className="px-4">|</span>
       </button>
     </div>
     {/* テキストエリア */}
