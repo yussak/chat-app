3c11e2ce05aa0ea69566ffd32afe588dfa8565f3
yussak <yusuke.sakuraba0213@gmail.com>
2025-04-27 10:18:26 +0900

    サンプルのe2eテスト修正

--- e2e/tests/example.spec.ts
+++ e2e/tests/example.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 
-test("トップページが開ける", async ({ page }) => {
-  await page.goto("http://app:3000");
-  await expect(page).toHaveTitle(/Next\.js/);
+test("ログインを要求される", async ({ page }) => {
+  await page.goto("http://client:3000");
+  await expect(page.locator("h1")).toHaveText("Please sign in");
 });
