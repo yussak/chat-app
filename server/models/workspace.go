1de984b32474a35378baa50701533fe7ddc3e61e
yussak <yusuke.sakuraba0213@gmail.com>
2025-04-30 21:37:28 +0900

    一時的なエラー対応

--- server/models/workspace.go
+++ server/models/workspace.go
@@ -13,6 +13,16 @@ type Workspace struct {
 	UpdatedAt time.Time `json:"updatedAt"`
 }
 
+// todo:エラー解消のため一時的にここに書いているので消す
+type Channel struct {
+	ID          int64     `json:"id"`
+	WorkspaceID int64     `json:"workspace_id"`
+	Name        string    `json:"name"`
+	IsPublic    bool      `json:"is_public"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
+
 type WorkspaceWithChannels struct {
 	Workspace
 	Channels []Channel `json:"channels"`
